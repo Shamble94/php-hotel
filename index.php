e505bab2bbe1e53803abe23d0af277e229791500
Shamble94 <fratus.nicholas@gmail.com>
2024-01-25 17:23:54 +0100

    Esercizio base completato

--- index.php
+++ index.php
@@ -1,11 +1,102 @@
+<?php
+
+    $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+   foreach ($hotels as $hotel){
+      if($hotel["parking"]){
+        $hotel["parking"] = "si";
+        
+      }else{
+        $hotel["parking"]  = "no";
+            
+      }
+   }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Desrizione</th>
+                            <th>Parcheggio</th>
+                            <th>Voto</th>
+                            <th>Distanza dal centro</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($hotels as $hotel) { ?>
+                            <tr>
+                                <td> 
+                                    <?php echo $hotel["name"];  ?>
+                                </td>
+                                <td> 
+                                    <?php echo $hotel["description"];  ?>
+                                </td>
+                                <td> 
+                                <?php echo $hotel["parking"];  ?>
+                                </td>
+                                <td> 
+                                    <?php echo $hotel["vote"];  ?>
+                                </td>
+                                <td> 
+                                    <?php echo $hotel["distance_to_center"];  ?>
+                                </td>
+                            </tr>
+                            <?php } ?>
+                        </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
